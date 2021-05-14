073e0d89bdd8fc17359a8c7dddd3ef1575fd20db
Vibhor007-dev <78255056+Vibhor007-dev@users.noreply.github.com>
2021-05-14 19:48:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,10 +47,10 @@ function setup() {
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
   
-  gameOver = createSprite(300,100);
+  gameOver = createSprite(trex.x+100,100);
   gameOver.addImage(gameOverImg);
   
-  restart = createSprite(300,140);
+  restart = createSprite(trex.x+100,140);
   restart.addImage(restartImg);
   
   gameOver.scale = 0.5;
@@ -71,7 +71,7 @@ function setup() {
 function draw() {
   //trex.debug = true;
   background(255);
-  text("Score: "+ score, 500,50);
+  text("Score: "+ score, trex.x+200,50);
   
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
@@ -116,15 +116,15 @@ function draw() {
       reset();
     }
   }
-  
-  
+  camera.position.x=trex.x;
+  camera.position.y=trex.height/2;
   drawSprites();
 }
 
 function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,120,40,10);
+    var cloud = createSprite(trex.x+500,120,40,10);
     cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
@@ -145,7 +145,7 @@ function spawnClouds() {
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);
+    var obstacle = createSprite(trex.x+500,165,10,40);
     //obstacle.debug = true;
     obstacle.velocityX = -(6 + 3*score/100);
     

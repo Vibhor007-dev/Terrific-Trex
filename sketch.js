386b2cb1f70d5fe55b324069cb7d7a51b1277812
Vibhor007-dev <78255056+Vibhor007-dev@users.noreply.github.com>
2021-05-14 20:52:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -76,6 +76,7 @@ function draw() {
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
+    image(groundImage,0,displayWidth*4,displayHeight,displayWidth*5)
   
     if(keyDown("space") && trex.y >= 159) {
       trex.velocityY = -12;
